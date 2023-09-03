c69a9af8d5e13ba4ddaaa51b589af61c89cfe48e
Dermot Treanor <dermot_treanor1@hotmail.com>
2023-09-03 15:39:01 +0100

    Added code to get all the date and times from the Alcoholics website. Still needs to be parsed and of course get data abou the locations

--- Selenium-practice.py
+++ Selenium-practice.py
@@ -1,8 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotInteractableException
+
+from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+# PATH = 'C:\Program Files (x86)\chromedriver.exe'
+# driver = webdriver.Chrome(PATH)
+
 driver = webdriver.Chrome()
 
-driver.get("https://youtube.com")
-time.sleep(15)
+driver.get("https://www.alcoholicsanonymous.ie/find-meeting/")
+driver.maximize_window()
+
+accept = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+accept.click()
+
+#Load all meetings
+load = driver.find_element(By.ID, "loadmore")
+try:
+    while True:
+        load.click()
+except ElementNotInteractableException:
+    pass
+
+
+#Get links to open meeting details
+links = driver.find_elements(By.LINK_TEXT, "Meeting Info")
+time.sleep(5)
+
+for link in links:
+    time.sleep(0.3)
+    link.click()
+    popup = driver.find_element(By.CLASS_NAME,"lity-opened")
+    right = popup.find_element(By.CLASS_NAME, "item_popup_inner_right")
+    ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    
+    print(right.text, "\n")
+
