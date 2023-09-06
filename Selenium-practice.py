9d113aa127a93263e5124fce33e67f3e6c5fcecf
Dermot Treanor <dermot_treanor1@hotmail.com>
2023-09-06 15:29:05 +0100

    The times item in the tuple value of meeting_date is no longer a long string but a dictionary with the days of meetings paired with the hours there are meetings on that day

--- Selenium-practice.py
+++ Selenium-practice.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
 
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -35,14 +35,40 @@ meeting_data = {}
 for link in links:
     time.sleep(0.3)
     link.click()
+    
+    #Open the popup with information for a particular meeting
     popup = driver.find_element(By.CLASS_NAME,"lity-opened")
     
-    place = popup.find_element(By.CLASS_NAME, "popup_title").text
+    #Get the name of the meeting and the address from the popup
+    title = popup.find_element(By.CLASS_NAME, "popup_title").text
     address = popup.find_element(By.CLASS_NAME, "item_address").text
-    times = popup.find_element(By.CLASS_NAME, "item_popup_inner_right").text
+    #times = popup.find_element(By.CLASS_NAME, "item_popup_inner_right").text
+    
+    times = {}
+    dates = popup.find_elements(By.CLASS_NAME, "times_list_item")
+    for date in dates:
+        day = date.find_element(By.CLASS_NAME, "day_label").text
+        times[day] = []
+        hours = date.find_elements(By.CLASS_NAME, "time_list_item")
+        
+        for hour in hours:
+            try:
+                meet_type = hour.find_element(By.CLASS_NAME, "meeting_description").text
+            except NoSuchElementException:
+                meet_type = None
+            if meet_type == "Physical":
+                times[day].append(hour.find_element(By.CLASS_NAME, "time").text)
+                
     ActionChains(driver).send_keys(Keys.ESCAPE).perform()
     
-    meeting_data[place] = (address, times)
+    meeting_data[title] = (address, times)
     #print(times.text, "\n")
 print(meeting_data)
+
+for i in meeting_data:
+    print(i, "\n\n", meeting_data[i][0], "\n\n\n", meeting_data[i][1], "\n\n\n\n\n\n")
+    
+    
+    
+    
 
